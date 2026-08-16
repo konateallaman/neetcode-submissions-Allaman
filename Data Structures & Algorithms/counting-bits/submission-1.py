65b4d2af55e8f8ac12fc10e7d226f85f69aea0cd
allaman Konate <113556544+konateallaman@users.noreply.github.com>
2026-08-16 06:53:47 -0500

    Add: counting-bits - submission-1

---
+++ Data Structures & Algorithms/counting-bits/submission-1.py
@@ -0,0 +1,9 @@
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        #dynamic prog
+        my_output=[0]
+        for i in range(1,n+1):
+            my_output.append(my_output[i>>1]+(i&1))
+        return my_output
+
+        
